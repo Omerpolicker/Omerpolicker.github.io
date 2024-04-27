fceaecc94908477805d5ccc256d3318441a9adb2
Omerpolicker <omerpolicker@gmail.com>
2024-04-27 19:10:46 +0300

    h

--- script13.js
+++ script13.js
@@ -1,40 +1,29 @@
-// script13.js
-
-// Function to handle visiting a pig
-// Function to handle visiting a pig and adding item to inventory
 function visitPig(chosenPig) {
-    // Update the text for the unchosen pig (if necessary)
-    var unchosenPig = chosenPig === 1 ? 2 : 1;
-    var unchosenPigElements = document.getElementsByClassName("unchosen-pig");
-    for (var i = 0; i < unchosenPigElements.length; i++) {
-        unchosenPigElements[i].innerText = "Pig " + unchosenPig;
+    console.log("Visit Pig function called with chosenPig: " + chosenPig);
+
+    // Hide the initial story text
+    document.getElementById("story-text").style.display = "none";
+
+    // Show the hidden part of the story based on the chosen pig
+    if (chosenPig === 1) {
+        console.log("Showing hidden story 1");
+        document.getElementById("hidden-story").style.display = "block";
+        document.getElementById("hidden-story2").style.display = "none"; // Hide other hidden story
+
+        // Add item to inventory based on chosen pig (straw)
+        var inventoryList = document.getElementById("inventory-list");
+        var itemToAdd = "<li><img src='https://static.vecteezy.com/system/resources/previews/008/513/979/original/red-drinking-straw-illustration-png.png' alt='Straw'> Straw</li>";
+        inventoryList.innerHTML += itemToAdd;
+    } else if (chosenPig === 2) {
+        console.log("Showing hidden story 2");
+        document.getElementById("hidden-story2").style.display = "block";
+        document.getElementById("hidden-story").style.display = "none"; // Hide other hidden story
+
+        // Add item to inventory based on chosen pig (sticks)
+        var inventoryList = document.getElementById("inventory-list");
+        var itemToAdd = "<li><img src='https://w7.pngwing.com/pngs/92/860/png-transparent-liquorice-stick-bundle-of-sticks-others-photography-root-spice.png' alt='Sticks'> Sticks</li>";
+        inventoryList.innerHTML += itemToAdd;
     }
-
-    // Add item to inventory based on chosen pig
-    var inventoryList = document.getElementById("inventory-list");
-    var itemToAdd = "";
-    switch(chosenPig) {
-        case 1:
-            itemToAdd = "<li><img src='https://static.vecteezy.com/system/resources/previews/008/513/979/original/red-drinking-straw-illustration-png.png' alt='Straw'> Straw</li>";
-            break;
-        case 2:
-            itemToAdd = "<li><img src='https://w7.pngwing.com/pngs/92/860/png-transparent-liquorice-stick-bundle-of-sticks-others-photography-root-spice.png' alt='Sticks'> Sticks</li>";
-            break;
-        case 3:
-            itemToAdd = "<li><img src='https://purepng.com/public/uploads/large/purepng.com-brickbuilding-materialbrickconstructionconcrete-materials-1421526461118aspkp.png' alt='Bricks'> Bricks</li>";
-            break;
-        default:
-            break;
-    }
-    inventoryList.innerHTML += itemToAdd;
-
-    // Log the chosen pig
-    console.log("Visited Pig " + chosenPig);
-}
-
-function visitPig(chosenPig) {
-    // Show the hidden part of the story
-    document.getElementById("hidden-story").style.display = "block";
     
     // Replace the placeholder with the name of the chosen pig
     var chosenPigName = chosenPig === 1 ? "Pig 1" : "Pig 2";
@@ -44,25 +33,44 @@ function visitPig(chosenPig) {
     var unchosenPigName = chosenPig === 1 ? "Pig 2" : "Pig 1";
     document.getElementById("unchosen-pig").innerText = unchosenPigName;
 
+    // Add the unchosen pig's button
+    var unchosenPigButtonPlaceholder = document.getElementById("unchosen-pig-button-placeholder");
+    unchosenPigButtonPlaceholder.innerHTML = ""; // Clear previous button if any
+    var unchosenPigButton = document.createElement("button");
+    unchosenPigButton.innerHTML = chosenPig === 1 ? "<img src='https://i.etsystatic.com/14036568/r/il/0675f9/3407921765/il_fullxfull.3407921765_cf6n.jpg' alt='Pig 2'> Pig 2" : "<img src='https://premiumpaintbynumbers.com/wp-content/uploads/2022/04/aesthetic-Pig-with-watermelon-paint-by-number.jpg' alt='Pig 1'> Pig 1";
+    unchosenPigButton.onclick = function() {
+        // Hide the story text
+        document.getElementById("story-text").style.display = "none";
 
-    // Add item to inventory based on chosen pig
-    var inventoryList = document.getElementById("inventory-list");
-    var itemToAdd = "";
-    switch(chosenPig) {
-        case 1:
-            itemToAdd = "<li><img src='https://static.vecteezy.com/system/resources/previews/008/513/979/original/red-drinking-straw-illustration-png.png' alt='Straw'> Straw</li>";
-            break;
-        case 2:
-            itemToAdd = "<li><img src='https://w7.pngwing.com/pngs/92/860/png-transparent-liquorice-stick-bundle-of-sticks-others-photography-root-spice.png' alt='Sticks'> Sticks</li>";
-            break;
-        case 3:
-            itemToAdd = "<li><img src='https://purepng.com/public/uploads/large/purepng.com-brickbuilding-materialbrickconstructionconcrete-materials-1421526461118aspkp.png' alt='Bricks'> Bricks</li>";
-            break;
-        default:
-            break;
-    }
-    inventoryList.innerHTML += itemToAdd;
+        // Hide both hidden stories
+        document.getElementById("hidden-story").style.display = "none";
+        document.getElementById("hidden-story2").style.display = "none";
 
-    // Log the chosen pig
-    console.log("Visited Pig " + chosenPig);
+        // Show hidden story based on the unchosen pig
+        if (chosenPig === 1) {
+            document.getElementById("hidden-story2").style.display = "block";
+            // Add sticks to inventory
+            var inventoryList = document.getElementById("inventory-list");
+            var itemToAdd = "<li><img src='https://w7.pngwing.com/pngs/92/860/png-transparent-liquorice-stick-bundle-of-sticks-others-photography-root-spice.png' alt='Sticks'> Sticks</li>";
+            inventoryList.innerHTML += itemToAdd;
+        } else if (chosenPig === 2) {
+            document.getElementById("hidden-story").style.display = "block";
+            // Add straw to inventory
+            var inventoryList = document.getElementById("inventory-list");
+            var itemToAdd = "<li><img src='https://static.vecteezy.com/system/resources/previews/008/513/979/original/red-drinking-straw-illustration-png.png' alt='Straw'> Straw</li>";
+            inventoryList.innerHTML += itemToAdd;
+        }
+    };
+    unchosenPigButtonPlaceholder.appendChild(unchosenPigButton);
 }
+
+// Add an event listener to the range input
+document.querySelector('.slider input[type="range"]').addEventListener('input', function() {
+    var sliderValue = parseInt(this.value);
+    if (sliderValue >= 60) {
+        // If the slider value is at least 20, show hidden-story3
+        document.getElementById("hidden-story2").style.display = "none";
+        document.getElementById("hidden-story3").style.display = "block";
+    }
+});
+
