dd4e9d92ef5a6c7b525998f120a56ac22c2fac89
Omerpolicker <omerpolicker@gmail.com>
2024-05-26 17:14:08 +0300

    s

---
+++ java/script.js
@@ -0,0 +1,33 @@
+function shalom () {
+    console.log ("Helloooo")
+
+}
+function showJustOne () {
+    document.getElementById ("one"). style.display = 'block';
+    document.getElementById ("two"). style.display = 'none';
+    document.getElementById ("three"). style.display = 'none';
+    document.getElementById ("next"). onclick = showJustTwo;
+}
+
+function showJustTwo () {
+    document.getElementById ("one"). style.display = 'none';
+    document.getElementById ("two"). style.display = 'block';
+    document.getElementById ("three"). style.display = 'none';
+    document.getElementById ("next"). onclick = showJustThree;
+}
+
+function showJustThree () {
+    document.getElementById ("one"). style.display = 'none';
+    document.getElementById ("two"). style.display = 'none';
+    document.getElementById ("three"). style.display = 'block'
+    document.getElementById ("next"). onclick = showJustOne;
+}
+
+function onLoad () {
+    document.getElementById ("next"). onclick = showJustOne;
+}
+
+function divClicked () {
+    document.getElementById("dorothy").addEventListener("click", divClicked);
+}
+
